9d6267da335155e454c7cb53c9ab727177d76a5e
TimmWerner <timm.werner@schmerreim.de>
2017-12-19 15:42:19 +0100

    get und pull requests für user, projects, abteilungen

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ let db = new sqlite3.Database('./db/zeiterfassung.db', sqlite3.OPEN_READWRITE, (
 //anfrage auf /api/projekte > gibt alle projekte zurück
 app.get("/api/roles/", (req,res) => {
 
+
   db.all('select * from role', [], (err, result) =>{
     console.log(result);
     res.send(result);
@@ -23,19 +24,6 @@ app.get("/api/roles/", (req,res) => {
     res.status(200).end();
   });
 
-
-/*
-  db.query('select * from role')
-		.then((result) => {
-			console.log(result);
-      res.status(200);
-			res.json(result[0]).end();
-		})
-		.catch((err) => {
-			console.log(err)
-			res.status(400).send(err).end()
-		})
-    */
 /*
   //zugriff auf DB
   var data = {"Mitarbeitername": "Hans Dampf"}; //hier kommt alles rein, was im body der response stehen soll
@@ -45,6 +33,54 @@ app.get("/api/roles/", (req,res) => {
   res.status(200).end();
   */
 });
+
+app.get("/api/mitarbeiter/:id", (req,res) => {
+
+  db.all('select * from USER where ID=?', [req.params.id], (err, result) =>{
+    console.log(result);
+    res.send(result);
+
+    res.status(200).end();
+  });
+});
+
+app.get("/api/projekte/:id", (req,res) => {
+
+  db.all('select * from PROJECT where ID=?', [req.params.id], (err, result) =>{
+    console.log(result);
+    res.send(result);
+
+    res.status(200).end();
+  });
+});
+
+app.get("/api/abteilungen/:id", (req,res) => {
+
+  db.all('select * from DEPARTMENT where ID=?', [req.params.id], (err, result) =>{
+    console.log(result);
+    res.send(result);
+
+    res.status(200).end();
+  });
+});
+
+
+app.post('/api/mitarbeiter/:id', (req, res) => {
+  req.params.id;
+db.run("INSERT into USER(Firstname,Lastname,LocationID,DepartmentID) VALUES (?,?,?,?)", [req.body.Firstname, req.body.Lastname,req.body.LocationID, req.body.DepartmentID]);
+  res.status(200).end();
+});
+
+app.post('/api/projekte/:id', (req, res) => {
+db.run("INSERT into PROJECT(ID,Name,Manager) VALUES (?,?,?)", [req.params.id, req.body.Name,req.body.Manager]);
+  res.status(200).end();
+});
+
+app.post('/api/abteilungen/:id', (req, res) => {
+db.run("INSERT into DEPARTMENT(ID,Name,Manager) VALUES (?,?,?)", [req.params.id, req.body.Name,req.body.Manager]);
+  res.status(200).end();
+});
+
 /*
 app.post('/api/projekte', (req, res) => {
 
